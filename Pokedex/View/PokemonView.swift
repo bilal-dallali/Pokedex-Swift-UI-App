d1e14126ddcee3b61b787470a680c63333b29f97
bilal-webdeveloper <dallali-bilal@hotmail.fr>
2022-11-29 19:48:23 +0100

    adding our pokemon cell

---
+++ Pokedex/View/PokemonView.swift
@@ -0,0 +1,22 @@
+//
+//  PokemonView.swift
+//  Pokedex
+//
+//  Created by Bilal Dallali on 29/11/2022.
+//
+
+import SwiftUI
+
+struct PokemonView: View {
+    var body: some View {
+        ZStack {
+            
+        }
+    }
+}
+
+struct PokemonView_Previews: PreviewProvider {
+    static var previews: some View {
+        PokemonView()
+    }
+}
